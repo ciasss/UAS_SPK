d21b8c5446f870e9f4959601718b8adee8d1903a
Sahriah <sahriahs86@gmail.com>
2024-12-16 00:58:04 +0800

    modify update delete alternatif

--- alternatif-hapus.php
+++ alternatif-hapus.php
@@ -1,5 +1,25 @@
 <?php
 require "include/conn.php";
+
 $id = $_GET['id'];
-mysqli_query($db, "delete from saw_alternatives where id_alternative='$id'");
+
+// Validasi input
+if (empty($id)) {
+    die("ID tidak valid!");
+}
+
+// Query untuk menghapus dari tabel saw_evaluations
+$sql_delete_evaluations = "DELETE FROM saw_evaluations WHERE id_alternative = '$id'";
+if (!mysqli_query($db, $sql_delete_evaluations)) {
+    die("Gagal menghapus data dari saw_evaluations: " . mysqli_error($db));
+}
+
+// Query untuk menghapus dari tabel saw_alternatives
+$sql_delete_alternatives = "DELETE FROM saw_alternatives WHERE id_alternative = '$id'";
+if (!mysqli_query($db, $sql_delete_alternatives)) {
+    die("Gagal menghapus data dari saw_alternatives: " . mysqli_error($db));
+}
+
+// Redirect setelah berhasil
 header("location:./alternatif.php");
+?>
